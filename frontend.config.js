88926399c1023de25ff8e6b048618573d9e2169b
Alexis Morel <alexis.morel@edu.itescia.fr>
2017-01-21 00:39:13 +0100

    Setup frontend build.

--- frontend.config.js
+++ frontend.config.js
@@ -18,7 +18,7 @@ module.exports = {
 	},
 	output: {
 		filename: "[name].js",
-		path: path.resolve(__dirname, "dist/frontend"),
+		path: path.resolve(__dirname, "public/js/app"),
 	},
 	resolve: {
 		extensions: [
