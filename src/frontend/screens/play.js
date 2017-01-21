9e51d781d3d0911a3ba934e6415e4bc42f17ff37
Alexis Morel <alexis.morel@edu.itescia.fr>
2017-01-21 10:22:07 +0100

    Transfer.

---
+++ src/frontend/screens/play.js
@@ -0,0 +1,15 @@
+import me from "melon";
+
+export default  me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function () {
+	},
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function () {
+	}
+});
